4aaef0abe737748fbcfa87adc644ff1dbdf24283
Pedro Renato <pedro.renato.mello@gmail.com>
2022-04-22 19:23:49 -0300

    Changed Script strategy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,23 +2,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return(
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-      <Script strategy="lazyOnload">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-              });
-          `}
-      </Script>
+        <Script strategy="afterInteractive">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+        </Script>
+
 
       <Component {...pageProps} />
     </>
