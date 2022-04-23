1adb9f352c5e1b540fc41b92d3e35d4b334baac7
Pedro Renato <pedro.renato.mello@gmail.com>
2022-04-23 11:09:31 -0300

    New Style and change on Links

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ const Home: NextPage = () => {
 
   const links = [
     {name: 'Instagram', href: 'https://www.instagram.com/hanellelysias_nutri/', icon: <AiOutlineInstagram className={iconClassName}/>},
-    {name: 'Consulta Online', href: 'https://wa.me/5521988878695', icon: <AiOutlineWhatsApp className={iconClassName}/>},
+    {name: 'Agendamento', href: 'https://wa.me/5521988878695', icon: <AiOutlineWhatsApp className={iconClassName}/>},
   ];
 
   return (
-    <div className='w-full h-full bg-gradient-to-r from-orange-300 to-orange-200 overflow-auto flex justify-center'>
+    <div className='w-full h-full bg-gradient-to-r from-slate-100 to-white overflow-auto flex justify-center'>
       <div className='lg:w-[380px] lg:h-[680px]  lg:pb-8 mt-12 flex flex-col justify-center'>
         <div className='flex grow flex-col items-center'>
-          <div className='w-[150px] h-[150px] p-1 rounded-full border-4 border-slate-100'>
+          <div className='w-[150px] h-[150px] p-1 rounded-full border-4 border-rose-300'>
             <Image
               src='/HLPhoto02.jpg'
               alt='logo'
@@ -28,22 +28,28 @@ const Home: NextPage = () => {
               className='rounded-full'
             />
           </div>
-          <p className='mt-4 font-medium text-center text-black w-64'>Hanelle Lysias | Nutricionista</p>
-          <p className='text-sm'>CRN4: 22100538/P</p>
+          <div className='w-32 h-32 z-10 flex absolute top-[172px]'>
+            <Image className='object-none' src='/HLLogoSmall.png' alt='Small logo'
+             width={300} height={300}/>
+          </div>
+          <p className='z-20 mt-4 font-medium text-center text-black w-64'>Hanelle Lysias | Nutricionista</p>
+          <p className='z-20 text-sm'>CRN4: 22100538/P</p>
           <p className='mt-4 font-serif font-medium text-center text-black/60 w-64'>&quot;Comece hoje o que te fará sentir orgulho amanhã&quot;</p>
-
+          
           <div className='mt-12 flex flex-col space-y-5'>
             {links.map((obj, key) => (
-              <Link href={obj.href} key={key} passHref>
-                <button className="w-60 bg-gradient-to-r from-slate-100 to-white hover:from-slate-200 hover:to-slate-100 shadow text-black font-normal py-2 px-10 rounded-full text-center flex justify-between items-center">
+              <a href={obj.href} key={key} target="_blank" rel="noopener noreferrer">
+                <button className="w-60 bg-gradient-to-r from-rose-100 to-rose-50 hover:from-slate-200 hover:to-slate-100 shadow text-black font-normal py-2 px-10 rounded-full text-center flex justify-between items-center">
                   {obj.name} 
                   {obj.icon}
                 </button>
-              </Link>
+              </a>
             ))}
           </div>
-        </div>
 
+          
+        </div>
+        
         <div className='flex justify-center pb-2 font-thin'>
           2022 <AiOutlineCopyrightCircle className="w-6 mt-1"/> por Nutri Lysias
         </div>
